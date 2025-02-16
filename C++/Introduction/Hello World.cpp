dbc8da008539f6a920d6eed1437e69011a074911
Vaibhav Maurya <mauryavaibhav2007@gmail.com>
2025-02-16 21:52:23 +0530

    create hello world

---
+++ C++/Introduction/Hello World.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl; // Print "Hello, World!" to the console using C++ style output
+    return 0; 
+}
