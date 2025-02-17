948a3770eebc9aba423851803fcb2954945f9f26
Vaibhav Maurya <mauryavaibhav2007@gmail.com>
2025-02-17 20:55:54 +0530

    Basic Data Types

---
+++ C++/Introduction/Basic Data Types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int a;      // Declare an integer variable a
+    long b;     // Declare a long integer variable b
+    char c;     // Declare a character variable c
+    float d;    // Declare a float variable d
+    double e;   // Declare a double variable e
+
+    // Read input for variables a, b, c, d, and e
+    cin >> a >> b >> c >> d >> e;
+
+    // Print the values of variables a, b, c, d, and e
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+
+    return 0;
+}
